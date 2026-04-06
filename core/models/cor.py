ccd82fcf256a052231b4231d74d79af0c5c22abc
Pauloartur-23 <pauloartur.ifc@gmail.com>
2026-04-06 13:37:48 -0300

    feat: criacao de api Modelo

--- core/models/cor.py
+++ core/models/cor.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Cor(models.Model):
-    nome = models.CharField(max_length=100)
+    nome = models.CharField(max_length=40)
 
     def __str__(self):
         return f'{self.nome({self.id})}'
