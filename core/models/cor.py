607b1a2ad074840e85cdbc6a5577263f79ed5c34
Pauloartur-23 <pauloartur.ifc@gmail.com>
2026-04-08 20:19:33 -0300

    feat: criacao da api Veiculo

--- core/models/cor.py
+++ core/models/cor.py
@@ -4,5 +4,9 @@ from django.db import models
 class Cor(models.Model):
     nome = models.CharField(max_length=40)
 
+    class Meta:
+        verbose_name = 'Cor'
+        verbose_name_plural = 'Cores'
+
     def __str__(self):
-        return f'{self.nome({self.id})}'
+        return f'{self.nome} ({self.id})'
